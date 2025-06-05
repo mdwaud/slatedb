1bc4435b4ccd37f4b1739f72018e526b8786b8d3
Mike Waud <mike.waud@sqlync.com>
2025-06-05 18:29:30 -0400

    export WriteOp match on it

--- slatedb/src/lib.rs
+++ slatedb/src/lib.rs
@@ -26,6 +26,7 @@ pub use fail_parallel;
 pub use object_store;
 
 pub use batch::WriteBatch;
+pub use batch::WriteOp;
 pub use cached_object_store::stats as cached_object_store_stats;
 pub use checkpoint::{Checkpoint, CheckpointCreateResult};
 pub use compactor::stats as compactor_stats;
